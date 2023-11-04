edb41d0de4d9e11101f1a9e9fc8a1cb07433f512
Juan Diego Salamanca Cuello <juan.salamanca@parqueexplora.org>
2023-11-04 16:50:49 -0500

    variables de entorno del cms configurada

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        COSMIC_BUCKET_SLUG: process.env.COSMIC_BUCKET_SLUG,
+        COSMIC_READ_KEY: process.env.COSMIC_READ_KEY,
+        COSMIC_WRITE_KEY: process.env.COSMIC_WRITE_KEY
+    }
+}
 
 module.exports = nextConfig
