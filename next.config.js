71ef4346242f3367ef10d0b76b1227f6666627ec
Juan Diego Salamanca Cuello <juan.salamanca@parqueexplora.org>
2023-11-04 16:56:39 -0500

    modo estricto de react configurado en next.config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        COSMIC_BUCKET_SLUG: process.env.COSMIC_BUCKET_SLUG,
-        COSMIC_READ_KEY: process.env.COSMIC_READ_KEY,
-        COSMIC_WRITE_KEY: process.env.COSMIC_WRITE_KEY
-    }
+  reactStrictMode: true,
+  env: {
+    COSMIC_BUCKET_SLUG: process.env.COSMIC_BUCKET_SLUG,
+    COSMIC_READ_KEY: process.env.COSMIC_READ_KEY,
+    COSMIC_WRITE_KEY: process.env.COSMIC_WRITE_KEY
+  }
 }
 
 module.exports = nextConfig
